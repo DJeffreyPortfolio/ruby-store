9db2af6a1f74d64734222c0c1a9916fa44d35aeb
David Jeffrey <D_Jeffrey87@yahoo.com>
2022-11-08 17:29:31 -0500

    Adding products and orders MVC

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,4 +1,19 @@
 class SimplePagesController < ApplicationController
   def index
+    @featured_product = Product.first
+  end
+  
+  def landing_page
+    @products = Product.limit(3)
+  end
+
+  def contact
+  end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   root 'simple_pages#index'
+  get 'simple_pages/landing_page'
+  get 'simple_pages/contact'
+  post 'simple_pages/thank_you'
 end
